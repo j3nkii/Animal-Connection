75ce1f900445f15d91f222a5e4b17d7e9422d582
Jacob Larson <jacob.larson55112@gmail.com>
2025-09-03 14:05:39 -0500

    reintroduce register api path

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -5,8 +5,9 @@ const {
 const encryptLib = require('../modules/encryption');
 const pool = require('../modules/pool');
 const userStrategy = require('../strategies/user.strategy');
-
 const router = express.Router();
+
+
 
 // Handles Ajax request for user information if user is authenticated
 router.get('/', rejectUnauthenticated, (req, res) => {
@@ -14,26 +15,26 @@ router.get('/', rejectUnauthenticated, (req, res) => {
   res.send(req.user);
 });
 
+
+
 // Handles POST request with new user data
 // The only thing different from this and every other post we've seen
 // is that the password gets encrypted before being inserted
+//Uncomment, and deploy to add new users.
+router.post('/register', (req, res, next) => {
+  const username = req.body.username;
+  const password = encryptLib.encryptPassword(req.body.password);
 
-
-        //Uncomment, and deploy to add new users.
-// router.post('/register', (req, res, next) => {
-//   const username = req.body.username;
-//   const password = encryptLib.encryptPassword(req.body.password);
-
-//   const queryText = `INSERT INTO "user" (username, password)
-//     VALUES ($1, $2) RETURNING id`;
-//   pool
-//     .query(queryText, [username, password])
-//     .then(() => res.sendStatus(201))
-//     .catch((err) => {
-//       console.log('User registration failed: ', err);
-//       res.sendStatus(500);
-//     });
-// });
+  const queryText = `INSERT INTO "user" (username, password)
+    VALUES ($1, $2) RETURNING id`;
+  pool
+    .query(queryText, [username, password])
+    .then(() => res.sendStatus(201))
+    .catch((err) => {
+      console.log('User registration failed: ', err);
+      res.sendStatus(500);
+    });
+});
 
 
 
@@ -45,11 +46,15 @@ router.post('/login', userStrategy.authenticate('local'), (req, res) => {
   res.sendStatus(200);
 });
 
+
+
 // clear all server session information about this user
 router.post('/logout', (req, res) => {
   // Use passport's built-in method to log out the user
   req.logout();
   res.sendStatus(200);
 });
+
+
 
 module.exports = router;
